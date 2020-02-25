4c49b2e84f314e8c91cb407a62fe6e764d711225
zheng2x2 <kelly.xx.zheng@gmail.com>
2020-02-26 00:24:53 +0900

    Updates

---
+++ precache-manifest.5e68dd2fbfa17876df3244c386b20e34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d25eef18691b45e9c3ad6e4bdf7fcfdb",
+    "url": "/react_movie/index.html"
+  },
+  {
+    "revision": "90632bce414ec06dc991",
+    "url": "/react_movie/static/css/main.1a6cbff0.chunk.css"
+  },
+  {
+    "revision": "1a4986dae91cf202cc45",
+    "url": "/react_movie/static/js/2.39eacd97.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react_movie/static/js/2.39eacd97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90632bce414ec06dc991",
+    "url": "/react_movie/static/js/main.7ae6394d.chunk.js"
+  },
+  {
+    "revision": "d0ba968992dab303697f",
+    "url": "/react_movie/static/js/runtime-main.f6c310b7.js"
+  }
+]);
